9e02182befe44e5bc46b051f05e05330ff4a868b
Frank Mochoge <mochogefrank01@gmail.com>
2024-11-21 23:28:39 +0300

    Day 001 of 100DaysOfPython, covered print, variables and coded a band name generator app

---
+++ 001/band_name_generator.py
@@ -0,0 +1,5 @@
+#Band Name generator
+print("Welcome to the band name generator")
+city_grew_up_in = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name is: " + city_grew_up_in + " " + pet_name)
